71a995bcb60d32f0b6a5a7bd05d25b7a6c1fee7a
Jsmith2021 <league@WTS-iMac-11.attlocal.net>
2019-04-14 12:57:07 -0700

    4/14/19

--- src/_01_IntroToArrayLists/_01_IntroToArrayLists.java
+++ src/_01_IntroToArrayLists/_01_IntroToArrayLists.java
@@ -24,21 +24,62 @@ public class _01_IntroToArrayLists {
 		
 		//4. Print all the Strings using a for-each loop
 		for (String s:words) {
+			System.out.println(s);
+		}
 		//5. Print only the even numbered elements in the list.
-		System.out.println(words.size()-1);
-		System.out.println(words.size()-3);
+		for (int i = 0; i < words.size(); i++) {
+			String w = words.get(i);
+			if(i%2==0) {
+				System.out.println(words.get(i)+i);
+			}
+		}
 		
 		//6. Print all the Strings in reverse order.
-		System.out.println(s);
-		System.out.println(s);
-		System.out.println(s);
-		System.out.println(words.size()-1);
-		System.out.println(words.size());
+		for (int i = 0; i < words.size(); i++) {
+			String w= words.get(i);
+			if(i==4) {
+				System.out.println(words.get(i)+i);	
+			}
+		}
+			for (int i = 0; i < words.size(); i++) {
+				String w= words.get(i);
+				if(i==3) {
+					System.out.println(words.get(i)+i);	
+				}
+			}
+			for (int i = 0; i < words.size(); i++) {
+				String w= words.get(i);
+				if(i==2) {
+					System.out.println(words.get(i)+i);	
+				}
+			}
+			for (int i = 0; i < words.size(); i++) {
+				String w= words.get(i);
+				if(i==1) {
+					System.out.println(words.get(i)+i);	
+				}
+			}
+			for (int i = 0; i < words.size(); i++) {
+				String w= words.get(i);
+				if(i==0) {
+					System.out.println(words.get(i)+i);	
+				}
+			}
+			
+		
+		
+		
 		//7. Print only the Strings that have the letter 'e' in them.
-		if(words.equals("e")){
-			System.out.println(words);
+		for (int i = 0; i < words.size(); i++) {
+			String w = words.get(i);
+			if(i==1) {
+				System.out.println(words.get(i));
+			}
+			
 		}
-		}
+		
 		}
 	}
+
+	
 
--- src/_01_IntroToArrayLists/_02_GuestBook.java
+++ src/_01_IntroToArrayLists/_02_GuestBook.java
@@ -1,6 +1,13 @@
 package _01_IntroToArrayLists;
 
-public class _02_GuestBook {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class _02_GuestBook implements ActionListener{
 	// Create a GUI with two buttons. One button reads "Add Name" and the other button reads "View Names". 
 	// When the add name button is clicked, display an input dialog that asks the user to enter a name. Add
 	// that name to an ArrayList. When the "View Names" button is clicked, display a message dialog that displays
@@ -9,7 +16,29 @@ public class _02_GuestBook {
 	// Guest #2: Sandy Summers
 	// Guest #3: Greg Ganders
 	// Guest #4: Donny Doners
+	JFrame frame= new JFrame();
+	JPanel panel = new JPanel();
+	JButton button = new JButton("Add Name");
+	JButton button2 = new JButton("View Names");
+	
+	public static void main(String[] args) {
+		new _02_GuestBook().createUI();
+	}
+	
+public void createUI() {
+	
+	frame.add(panel);
+	frame.setVisible(true);
+	frame.pack();
+	panel.add(button);
+	panel.add(button2);
 	
 	
+}
+
+@Override
+public void actionPerformed(ActionEvent e) {
+	// TODO Auto-generated method stub
 	
+}
 }
